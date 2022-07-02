079aa34c8b371c92c0446eea566c886d7200bc67
yanadev <1245485816@qq.com>
2022-07-02 19:10:58 +0800

    project init

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "parser": "@babel/eslint-parser",
+  "parserOptions": {
+    "requireConfigFile": false
+  },
+  "extends": "airbnb-base",
+  "env": {
+    "browser": true,
+    "node": true
+  }
+};
