6a5257a573a211a8676333b26798bbae28684ea8
yanadev <1245485816@qq.com>
2022-07-03 18:57:24 +0800

    test: wbepack.base test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,7 +4,7 @@ describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base');
   // console.log(baseConfig);
   it('entry', () => {
-    assert.equal(baseConfig.entry.index, '/home/runner/work/builder-webpack/builder-webpack/test/smoke/template/src/index/index.jsx');
-    assert.equal(baseConfig.entry.search, '/home/runner/work/builder-webpack/builder-webpack/test/smoke/template/src/search/index.jsx');
+    assert.equal(baseConfig.entry.index.indexOf('builder-webpack/test/smoke/template/src/index/index.jsx') > -1, true);
+    assert.equal(baseConfig.entry.search.indexOf('builder-webpack/test/smoke/template/src/search/index.jsx') > -1, true);
   });
 });
